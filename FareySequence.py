9a67070b3af33ee5aea6a393b7d5ecf719d3d288
Kadin S <99530266+KadinSchultz@users.noreply.github.com>
2023-12-04 15:32:11 +1000

    Delete FareySequence.py

--- FareySequence.py
+++
@@ -1,6 +0,0 @@
-# Problem: Farey Sequence
-# Link: https://code.golf/farey-sequence#python
-# Solutions characters: 104
-
-r=range
-print('\n'.join([*zip(*sorted({j/i:f'{j}/{i}'for i in r(50,0,-1)for j in r(i+1)}.items()))][1]))
