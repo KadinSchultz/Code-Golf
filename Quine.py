1ee6fa7e7788b646744d2e4b98aaf9908188157d
Kadin S <99530266+KadinSchultz@users.noreply.github.com>
2023-12-04 15:29:40 +1000

    Delete Quine.py

--- Quine.py
+++
@@ -1,6 +0,0 @@
-# Problem: Quine
-# Link: https://code.golf/quine#python
-# Solutions characters: 32
-# Note: The blank/whitespace row after the code is required for this to run properly on the Code Golf website
-
-x='x=%r;print(x%%x)';print(x%x)
