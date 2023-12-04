c64f6a9d755e9c580496112fb046f02f4e8613df
Kadin S <99530266+KadinSchultz@users.noreply.github.com>
2023-12-04 15:49:08 +1000

    Delete Fractions.py

--- Fractions.py
+++
@@ -1,7 +0,0 @@
-# Problem: Fractions
-# Link: https://code.golf/fractions#python
-# Solutions characters: 103
-
-import sys
-from fractions import*
-for i in sys.argv[1:]:print('%d/%d'%(Fraction(i).as_integer_ratio()))
